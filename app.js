c837b54b84c822330c95638f3f314630b400d973
Alexander Krastev <a.krastev@tinqin.com>
2023-02-07 16:05:39 +0200

    added headers

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
   next();
